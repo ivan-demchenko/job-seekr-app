7ae3a9eca3272f76ffd4d6aca694ed17bc020abf
Ivan Demchenko <idemchenko.js@gmail.com>
2026-05-15 10:41:37 +0200

    chore: cleanup

--- packages/api/src/utils.ts
+++ packages/api/src/utils.ts
@@ -1,35 +1,8 @@
-import { format, formatISO, getTime } from "date-fns";
-import DOMPurify from "dompurify";
-import { Marked } from "marked";
-
-const marked = new Marked();
+import { format } from "date-fns";
 
 export const printDate = (timestamp?: number): string => {
   if (!timestamp) {
     return format(new Date(), "dd MMM yyyy");
   }
   return format(new Date(timestamp), "dd MMM yyyy");
-};
-
-export const printDateTime = (timestamp?: number): string => {
-  if (!timestamp) {
-    return format(new Date(), "dd MMM yyyy, HH:mm");
-  }
-  return format(new Date(timestamp), "dd MMM yyyy, HH:mm");
-};
-
-export const dateToTimestamp = (str: string): number => {
-  return getTime(str);
-};
-
-export const timestampToISO = (timestamp: number): string => {
-  return formatISO(new Date(timestamp)).substring(0, "yyyy-MM-ddThh:mm".length);
-};
-
-export const getCurrentTimestamp = (): number => {
-  return getTime(new Date());
-};
-
-export const renderMD = (str: string): string => {
-  return DOMPurify.sanitize(marked.parse(str, { async: false }));
 };
